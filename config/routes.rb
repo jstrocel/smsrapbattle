dbfd8e12de4a8905f5054f5461bc355314d472d6
James Strocel <James.Strocel@gmail.com>
2012-03-24 15:23:50 -0700

    Changed frontpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Smsrapbattle::Application.routes.draw do
   resources :users
+  root :to => "users#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
