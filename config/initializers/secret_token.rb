4484e7c14b44e0420aa000427ae55c4241c97b1f
James Strocel <James.Strocel@gmail.com>
2012-03-24 15:12:24 -0700

    Second Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smsrapbattle::Application.config.secret_token = '03977277758ee562bb85e34b785d14b42e163083a9adb1776f65e095c4068082d65700d50152c36aa2a451b03d63720f527417e4b79802458d31d5a96130c155'
